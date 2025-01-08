62e68fd8f6581c01cf2671f6864bcc34b0748057
tapesh1134 <tapeshs1134@gmail.com>
2025-01-09 00:07:45 +0530

    configured

--- src/app.js
+++ src/app.js
@@ -13,5 +13,11 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// Routes
+import userRoutes from "./routes/user.routes.js"
+
+
+// Routes declaration
+app.use("/api/v1/users",userRoutes)
 
 export {app}
