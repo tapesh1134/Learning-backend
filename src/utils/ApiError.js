a9f8aefdbf2b4e10e5df44f3e8b22be4933b7f7a
tapesh1134 <tapeshs1134@gmail.com>
2025-01-08 10:07:50 +0530

    set up user and video schema

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -6,7 +6,7 @@ class ApiError extends Error {
         stack = ""
     ) {
         super(message)
-        this.statusCosde = statusCode
+        this.statusCode = statusCode
         this.data = null
         this.message = message
         this.success = false;
