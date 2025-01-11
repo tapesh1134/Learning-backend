57dc9033e7f9dc019061113d6a36e2181dbbe172
tapesh1134 <tapeshs1134@gmail.com>
2025-01-11 14:57:35 +0530

    complete user controller and auth

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,21 @@ import { User } from "../models/user.model.js";
 import { uploadCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessAndRefereshTokens = async (userId) => {
+    try {
+        const user = User.findById(userId)
+        const accessToken = User.generateAccessToken()
+        const refreshToken = User.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+
+        return { accessToken, refreshToken }
+    } catch {
+        throw new ApiError(500, "Somethind went wrong while generating referesh and access token")
+    }
+}
+
 const registerUser = asyncHandler(async (req, res) => {
     // Steps for user register
     // 1. get user details from frontend
@@ -17,7 +32,7 @@ const registerUser = asyncHandler(async (req, res) => {
     // 9. return res
 
     // Step 2
-    const {fullName, email, username, password } = req.body
+    const { fullName, email, username, password } = req.body
     if (
         [fullName, email, username, password].some(
             (fields) => fields?.trim() === ""
@@ -36,7 +51,13 @@ const registerUser = asyncHandler(async (req, res) => {
 
     // Step 4
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar required")
@@ -73,11 +94,93 @@ const registerUser = asyncHandler(async (req, res) => {
         new ApiResponse(200, createdUser, "User registered successfully")
     )
 });
-const loginUser = asyncHandler(async (req,res) =>{
+
+const loginUser = asyncHandler(async (req, res) => {
+    // req body -> data
+    // username or email
+    // find the user
+    // password check
+    // access and referesh token
+    // send cookie
+
+    const { email, username, password } = req.body
+    console.log(email);
+
+    if (!username && !email) {
+        throw new ApiError(400, "Username or Email is requires")
+    }
+
+    const user = User.findOne({
+        $or: [{ username, email }]
+    })
+
+    if (!user) {
+        throw new ApiError(404, "User does not exist")
+    }
+
+    const isPasswordValid = await User.isPasswordCorrect(password)
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid credentials")
+    }
+
+    const { accessToken, refreshToken } = await User.generateAccessAndRefereshTokens()
+
+    const loggedInUser = User.findById(user._id).select("-password -refreshToken")
+
+    const option = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, option)
+        .cookie("refreshToken", refreshToken, option)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    user, loggedInUser, refreshToken
+                },
+                "User logged in successfully"
+            )
+        )
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $usset: {
+                refreshToken: 1
+            }
+        }, {
+        new: true
+    }
+    )
+    const option = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+        .status(200)
+        .cookie("accessToken", option)
+        .cookie("refreshToken", option)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                },
+                "User loggedOut successfully"
+            )
+        )
 
 })
-const logOut = asyncHandler(async (req,res) => {
-    
-})
 
-export { registerUser };
+export {
+    registerUser,
+    loginUser,
+    logoutUser
+};
